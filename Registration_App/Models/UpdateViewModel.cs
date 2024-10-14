3c851308de20aa09b8597d8dec83b93a6b13cc60
Yashwanth.Akuthota <Yashwanth.Akuthota@DMCMP651>
2024-10-14 15:05:32 +0530

    commit

---
+++ Registration_App/Models/UpdateViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+namespace Registration_App.Models
+{
+    public class UpdateViewModel
+    {
+        //Id is untouched,it is there for fetching data
+        //Id is untouched,it is there for fetching data
+        [Key]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+        public List<string> Skills { get; set; } = new List<string>();
+    }
+}
+
+
+
+
+
+
+
